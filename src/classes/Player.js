fc0d796267293de5679d11f569dc26cfb3af9fd9
Nathan <nluciom04@hotmail.com>
2025-08-21 23:03:37 -0300

    player moved to class

---
+++ src/classes/Player.js
@@ -0,0 +1,50 @@
+module.exports.Player = class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.velocityy = 0;
+        this.velocityLimit = -70;
+        this.jumpHeight = 200;
+        this.jumpTimer = 0;
+        this.radius = 25;
+        this.color = "black";
+    }
+    boundaries(canvas) {
+        const canvasTop = 0;
+        if (this.y > canvas.height - this.radius) {
+            this.y = canvas.height - this.radius;
+            this.velocityy = 0;
+        }
+        if (this.y < canvasTop + this.radius) {
+            this.y = canvasTop + this.radius;
+            this.velocityy = 0;
+        }
+    }
+    _velocityLimit() {
+        if (this.velocityy <= this.velocityLimit) {
+            this.velocityy = this.velocityLimit;
+        }
+    }
+    physics(gravity, deltaTime) {
+        this.velocityy += gravity * deltaTime;
+        this.y += this.velocityy * deltaTime;
+        this._velocityLimit();
+    }
+    jump(timeStamp) {
+        if (timeStamp - this.jumpTimer > 100) {
+            this.velocityy -= this.jumpHeight;
+            this.jumpTimer = timeStamp;
+        } else {
+            return;
+        }
+    }
+    draw(canvas, context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        context.closePath();
+        context.fillStyle = this.color;
+        context.fill();
+
+        this.boundaries(canvas);
+    }
+};
