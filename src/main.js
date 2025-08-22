00a8725692ff2da51612792ca04c43b512edf30d
Nathan <nluciom04@hotmail.com>
2025-08-22 01:19:34 -0300

    collision stops the game

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ class Game {
         this.context = canvas.getContext("2d");
         this.gravity = 9.8;
         this.animationFrame;
+        this.isGameOver = false;
         this.timeStamp = 0;
         this.deltaTime;
         this.player = new Player(this.canvas.width / 4, this.canvas.height / 3);
@@ -19,14 +20,23 @@ class Game {
         );
     }
     drawLoop(currentTime) {
+        if (this.isGameOver) return;
+
         this.deltaTime = (currentTime - this.timeStamp) / 100;
         this.timeStamp = currentTime;
 
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // Player
         this.player.physics(this.gravity, this.deltaTime);
-        this.player.collision(this.block);
+        const collision = this.player.collision(this.block);
+        if (collision) {
+            window.cancelAnimationFrame(this.animationFrame);
+            this.isGameOver = true;
+        }
         this.player.draw(this.canvas, this.context);
 
+        // Block
         this.block.move(this.deltaTime, this.timeStamp);
         this.block.draw(this.canvas, this.context);
 
