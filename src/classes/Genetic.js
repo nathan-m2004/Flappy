4b886d0ee1d1684a0ab6d4d0ce981b704c902dae
Nathan <nluciom04@hotmail.com>
2025-08-25 01:50:48 -0300

    adding crossover and fitness

--- src/classes/Genetic.js
+++ src/classes/Genetic.js
@@ -1,6 +1,12 @@
 const { Matrix } = require("./Matrix.js");
 const { Player } = require("./Player.js");
 
+function map(value, min_in, max_in, min_out, max_out) {
+    return (
+        ((value - min_in) * (max_out - min_out)) / (max_in - min_in) + min_out
+    );
+}
+
 class NeuralNetwork {
     constructor(inputNodes, hiddenNodes, outputNodes) {
         this.inputNodes = inputNodes;
@@ -15,6 +21,56 @@ class NeuralNetwork {
         this.bias_o.randomize();
         this.weights_ih.randomize();
         this.weights_ho.randomize();
+    }
+
+    crossover(brain) {
+        let child = new NeuralNetwork(
+            this.inputNodes,
+            this.hiddenNodes,
+            this.outputNodes
+        );
+
+        for (let r = 0; r < this.bias_h.rows; r++) {
+            for (let c = 0; c < this.bias_h.cols; c++) {
+                if (Math.random() > 0.5) {
+                    child.bias_h.data[r][c] = brain.bias_h.data[r][c];
+                } else {
+                    child.bias_h.data[r][c] = this.bias_h.data[r][c];
+                }
+            }
+        }
+
+        for (let r = 0; r < this.bias_o.rows; r++) {
+            for (let c = 0; c < this.bias_o.cols; c++) {
+                if (Math.random() > 0.5) {
+                    child.bias_o.data[r][c] = brain.bias_o.data[r][c];
+                } else {
+                    child.bias_o.data[r][c] = this.bias_o.data[r][c];
+                }
+            }
+        }
+
+        for (let r = 0; r < this.weights_ho.rows; r++) {
+            for (let c = 0; c < this.weights_ho.cols; c++) {
+                if (Math.random() > 0.5) {
+                    child.weights_ho.data[r][c] = brain.weights_ho.data[r][c];
+                } else {
+                    child.weights_ho.data[r][c] = this.weights_ho.data[r][c];
+                }
+            }
+        }
+
+        for (let r = 0; r < this.weights_ih.rows; r++) {
+            for (let c = 0; c < this.weights_ih.cols; c++) {
+                if (Math.random() > 0.5) {
+                    child.weights_ih.data[r][c] = brain.weights_ih.data[r][c];
+                } else {
+                    child.weights_ih.data[r][c] = this.weights_ih.data[r][c];
+                }
+            }
+        }
+
+        return child;
     }
 
     feedForward(inputArray) {
@@ -33,11 +89,20 @@ class NeuralNetwork {
 }
 
 module.exports.PlayerGenetic = class PlayerGenetic {
-    constructor(canvas) {
-        this.brain = new NeuralNetwork(5, 3, 1);
+    constructor(canvas, brain) {
+        if (brain) {
+            this.brain = brain;
+        } else {
+            this.brain = new NeuralNetwork(5, 3, 1);
+        }
         this.player = new Player(canvas.width / 4, canvas.height / 3, 0.45);
         this.score = 0;
         this.isDead = false;
+        this.fitness;
+    }
+    calculateFitness() {
+        const scoreNormalized = map(this.score, 0, 50, -1, 0);
+        this.fitness = scoreNormalized;
     }
     addScore() {
         if (this.isDead) return;
@@ -46,14 +111,6 @@ module.exports.PlayerGenetic = class PlayerGenetic {
     }
     think(block, canvas, timeStamp) {
         if (block && !this.isDead) {
-            function map(value, min_in, max_in, min_out, max_out) {
-                return (
-                    ((value - min_in) * (max_out - min_out)) /
-                        (max_in - min_in) +
-                    min_out
-                );
-            }
-
             let inputs = [];
             inputs[0] = map(
                 this.player.velocityy,
